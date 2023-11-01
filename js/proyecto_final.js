41c3d827dd54c148e9b950ff71d75d7cd7707d80
Agustina Pedernera <agustinapedernera97@gmail.com>
2023-11-01 17:36:52 -0300

    Cargamos el proyecto en un repo

---
+++ js/proyecto_final.js
@@ -0,0 +1,10 @@
+const botonAporte = document.querySelector('.boton-central');
+const botonDona = document.querySelector('.dona');
+const botonCompra = document.querySelector('.compra');
+
+
+
+botonAporte.addEventListener('click', () => {});
+botonDona.addEventListener('click', () => {
+console.log('funciona el boton')});
+botonCompra.addEventListener('click', () => {console.log('funciona el boton')});

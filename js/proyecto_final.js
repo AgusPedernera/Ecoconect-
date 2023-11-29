c6f17c2bc93f19d714a7c8c6c49525da0a748fdb
Sharon Puente <sharoncpuente@gmail.com>
2023-11-28 22:24:56 -0300

    Agrega el formulario

--- js/proyecto_final.js
+++ js/proyecto_final.js
@@ -8,3 +8,26 @@ botonAporte.addEventListener('click', () => {});
 botonDona.addEventListener('click', () => {
 console.log('funciona el boton')});
 botonCompra.addEventListener('click', () => {console.log('funciona el boton')});
+
+// Formulario
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Llamamos a mostrarPregunta al cargar la página para establecer el estado inicial
+    mostrarPregunta(2);
+    mostrarPregunta(4);
+});
+
+function mostrarPregunta(numero) {
+    var pregunta = document.getElementById("pregunta" + numero);
+    var respuesta = document.getElementById("respuesta" + numero);
+
+    var radioSi = document.getElementById("pregunta" + (numero - 1) + "Si");
+
+    if (radioSi.checked) {
+        pregunta.classList.remove("hidden");
+    } else {
+        pregunta.classList.add("hidden");
+        // Limpiamos la respuesta cuando se oculta la pregunta
+        respuesta.value = "";
+    }
+}

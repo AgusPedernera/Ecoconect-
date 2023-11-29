2124e5a9c9501a446234fbc6ca51b0328342c2f7
AgusPedernera <agustinapedernera97@gmail.com>
2023-11-28 23:41:32 -0300

    Arreglamos js y css

--- js/proyecto_final.js
+++ js/proyecto_final.js
@@ -29,3 +29,37 @@ boton.addEventListener('click',
         }
     }
 ); 
+
+const botonAporte = document.querySelector('.boton-central');
+const botonDona = document.querySelector('.dona');
+const botonCompra = document.querySelector('.compra');
+
+
+
+botonAporte.addEventListener('click', () => {});
+botonDona.addEventListener('click', () => {
+console.log('funciona el boton')});
+botonCompra.addEventListener('click', () => {console.log('funciona el boton')});
+
+// Formulario
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Llamamos a mostrarPregunta al cargar la página para establecer el estado inicial
+    mostrarPregunta(2);
+    mostrarPregunta(4);
+});
+
+function mostrarPregunta(numero) {
+    var pregunta = document.getElementById("pregunta" + numero);
+    var respuesta = document.getElementById("respuesta" + numero);
+
+    var radioSi = document.getElementById("pregunta" + (numero - 1) + "Si");
+
+    if (radioSi.checked) {
+        pregunta.classList.remove("hidden");
+    } else {
+        pregunta.classList.add("hidden");
+        // Limpiamos la respuesta cuando se oculta la pregunta
+        respuesta.value = "";
+    }
+}

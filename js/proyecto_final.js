74ca14fd1762a3002b33d0a2636f83218d6311d0
AgusPedernera <agustinapedernera97@gmail.com>
2023-12-12 20:27:26 -0300

    borra el menu hamb de categoria en dona y compra

--- js/proyecto_final.js
+++ js/proyecto_final.js
@@ -9,26 +9,26 @@
 // botonCompra.addEventListener('click', () => {console.log('funciona el boton')});
 
 
-const categoria = document.querySelector('.cartas');
-const boton = document.querySelector('img[alt="menu"]');
+// const categoria = document.querySelector('.cartas');
+// const boton = document.querySelector('img[alt="menu"]');
 
 
-const nav = document.querySelector('header nav');
+// const nav = document.querySelector('header nav');
 
-let estado = 'up';
-boton.addEventListener('click', 
-    function (){
-        categoria.classList.toggle('desplegado');
-        if( estado == 'up' ){
-            boton.src = 'img/cruz.png';
-            estado = 'down';
-        }
-        else{
-            boton.src = 'img/hamburg 1.png';
-            estado = 'up';
-        }
-    }
-); 
+// let estado = 'up';
+// boton.addEventListener('click', 
+//     function (){
+//         categoria.classList.toggle('desplegado');
+//         if( estado == 'up' ){
+//             boton.src = 'img/cruz.png';
+//             estado = 'down';
+//         }
+//         else{
+//             boton.src = 'img/hamburg 1.png';
+//             estado = 'up';
+//         }
+//     }
+// ); 
 
 const botonAporte = document.querySelector('.boton-central');
 const botonDona = document.querySelector('.dona');
